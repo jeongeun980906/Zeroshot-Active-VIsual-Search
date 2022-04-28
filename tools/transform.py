81291b9a4f9196945066dcd734dcc731b648af1e
Jeongeun <baro0906@korea.ac.kr>
2022-04-28 22:46:58 +0900

    detection

--- tools/transform.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-def cornerpoint_projection(cornerpoints):
-    res = []
-    for e,c in enumerate(cornerpoints):
-        if e%4==0 or e%4==1:
-            res.append([c[0],c[2]])
-    return res
