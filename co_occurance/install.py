b297d3a54a46b658d0717bd2a1485d65652aec9e
jeongeun <baro0906@korea.ac.kr>
2022-09-19 14:29:05 +0900

    project page

--- co_occurance/install.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
-model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large")
